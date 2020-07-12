54f1155f2453d8155a710179ff3614aefb9cb4ed
Perla SC <perlascabrera@gmail.com>
2020-07-12 10:12:39 -0500

    add event

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const iconError = document.getElementById('icon--error')
 const textError = document.getElementById('text--error')
 const arrow = document.getElementById('arrow')
+
+arrow.addEventListener('click', () => {
+    iconError.classList.remove('hidden')
+    textError.classList.remove('hidden')
+})
