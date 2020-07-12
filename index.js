b0dfc03d331ffdb36c9af706fdf88c051ed94a8c
Perla SC <perlascabrera@gmail.com>
2020-07-12 17:33:22 -0500

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const iconError = document.getElementById('icon--error')
 const textError = document.getElementById('text--error')
 const arrow = document.getElementById('arrow')
+const input = document.getElementById('input')
 
 arrow.addEventListener('click', () => {
     iconError.classList.remove('hidden')
     textError.classList.remove('hidden')
+    input.classList.remove('border')
+    input.classList.add('border--error')
+    arrow.classList.add('arrow--error')
 })
